e37a3379b8a7bcb928e536ad28af135721ca9836
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-15 23:10:06 +0000

    created a function that generates a random 6 digit code

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -30,6 +37,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`This app is listening on port ${PORT}`);
 });
+
+function generateRandomString() {
+ return Math.random().toString(20).substr(2, 6);
+
+}
