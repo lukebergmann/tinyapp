2e8add757a8a8b0f279c04f9e58a68e1e4fde286
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-16 20:22:06 +0000

    updated server

--- express_server.js
+++ express_server.js
@@ -1,11 +1,17 @@
+// THIS PAGE IS THE MAIN SERVER //
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//This tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs");
 
+// Install morgan
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,25 +22,25 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Route to render the urls_index template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+// Route to render the urls_show template
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
+// The root path
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -51,10 +57,12 @@ app.post("/urls", (req, res) => {
   
 });
 
+// App is listening on port 8080
 app.listen(PORT, () => {
   console.log(`This app is listening on port ${PORT}`);
 });
 
+// A function to generate a random 6 character code (Alphanumeric and Numerical mixed)
 function generateRandomString() {
  return Math.random().toString(36).substr(2, 6);
 
