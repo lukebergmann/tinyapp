6bb3ed2e8844c647bca8ca9cf956614ef4a23dc1
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-19 06:23:54 +0000

    linted code

--- express_server.js
+++ express_server.js
@@ -4,31 +4,29 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-// const cookieParser = require("cookie-parser");
-// app.use(cookieParser());
 const cookieSession = require("cookie-session");
 app.use(cookieSession({
   name: "tinyapp",
   keys: ['first key', 'second key']
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const bcrypt = require('bcryptjs');
 
-//This tells the Express app to use EJS as its templating engine
+//This tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs");
 
-// Install morgan
+// Install morgan.
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
-// GLOBAL URL DATABASE
+// GLOBAL URL DATABASE.
 const urlDatabase = {};
 
-// GLOBAL USER DATABASE 
+// GLOBAL USER DATABASE. 
 const users = {};
 
-//Helper Function that allowes logged in user to only see their URLS
+//Helper Function that allowes logged in user to only see their URLS.
 const urlsForUser = function (userIDFromCookies) {
   const urlResult = {};
   for (let key in urlDatabase) {
@@ -40,7 +38,7 @@ const urlsForUser = function (userIDFromCookies) {
   return urlResult;
 };
 
-// Helper function that authenticates a user if users login passes all the requirements 
+// Helper function that authenticates a user if users login passes all the requirements.
 const authenticateUser = function (users, password, email) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -52,34 +50,34 @@ const authenticateUser = function (users, password, email) {
   return false;
 };
 
-// Helper Function to Generate a random 6 character code
+// Helper Function to Generate a random 6 character code.
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 };
 
 
-// The root path
+// The root path.
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// GET to render My URLs
+// GET to render My URLs.
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = { urls: urlsForUser(user_id), user: users[user_id] };
   res.render("urls_index", templateVars);
 });
 
-//POST new url
+//POST new url.
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString(req);
-  urlDatabase[shortURL] = { longURL: "", userID: req.session.user_id }
+  urlDatabase[shortURL] = { longURL: "", userID: req.session.user_id };
   let longURL = req.body.longURL;
   urlDatabase[shortURL].longURL = longURL;
   res.redirect(`/urls`);
 });
 
-// GET to create new URL
+// GET to create new URL.
 app.get("/urls/new", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = {
@@ -88,24 +86,24 @@ app.get("/urls/new", (req, res) => {
   if (users[user_id]) {
     res.render("urls_new", templateVars);
   } else {
-    res.redirect("/login")
+    res.redirect("/login");
   }
 });
 
-// GET to render the urls_show template
+// GET to render the urls_show template.
 app.get("/urls/:shortURL", (req, res) => {
   const user_id = req.session.user_id;
   if (users[user_id]) {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[user_id] };
     res.render("urls_show", templateVars);
   } else {
-    res.redirect("/404")
+    res.redirect("/404");
   }
 });
 
-//POST to edit a URL
+//POST to edit a URL.
 app.post("/urls/:shortURL", (req, res) => {
-  let longURL = req.body.longURL
+  let longURL = req.body.longURL;
   let shortURL = req.params.shortURL;
   const user_id = req.session.user_id;
   if (users[user_id]) {
@@ -114,18 +112,18 @@ app.post("/urls/:shortURL", (req, res) => {
   }
 });
 
-// GET to transfer long URL into shortURL
+// GET to transfer long URL into shortURL.
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
-// POST to delete a URL
+// POST to delete a URL.
 app.post("/urls/:shortURL/delete", (req, res) => {
   const user_id = req.session.user_id;
   if (users[user_id]) {
     delete urlDatabase[req.params.shortURL];
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
     console.log("You Do Not Have Permission To See This!");
   }
@@ -135,59 +133,55 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//GET to login
+//GET to login.
 app.get("/login", (req, res) => {
-  // const user_id = req.cookies["user_id"]
-  const user_id = req.session.user_id
+  const user_id = req.session.user_id;
   const templateVars = { user: users[user_id] };
-  res.render("urls_login", templateVars)
+  res.render("urls_login", templateVars);
 });
 
-//POST to login
+//POST to login.
 app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   if (!email || !password) {
     res.redirect("/403-email");
   }
-  // password/email not in database
   else if (!checkEmail(users, email)) {
-    res.redirect("/403-email")
+    res.redirect("/403-email");
   }
   else {
-    // define user
     const user = authenticateUser(users, password, email);
     if (user) {
-      // res.cookie("user_id", user.id);
       req.session.user_id = user.id;
       res.redirect("/urls");
     }
     else {
-      res.status(403).send("Incorrect Password")
+      res.status(403).send("Incorrect Password");
     }
   }
 });
 
-//POST to logout
+//POST to logout.
 app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
-//GET to logout
+//GET to logout.
 app.get("/logout", (req, res) => {
   req.session = null;
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
-// GET to registration page
+// GET to registration page.
 app.get("/register", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = { user: users[user_id] };
-  res.render("urls_register", templateVars)
+  res.render("urls_register", templateVars);
 });
 
-//POST to register new users in user database
+//POST to register new users in user database.
 app.post("/register", (req, res) => {
   let newID = generateRandomString();
   let newEmail = req.body.email;
@@ -195,7 +189,7 @@ app.post("/register", (req, res) => {
   if (!checkEmail(users, newEmail)) {
     bcrypt.genSalt(10)
       .then((salt) => {
-        return bcrypt.hash(newPassword, salt)
+        return bcrypt.hash(newPassword, salt);
       })
       .then((hash) => {
         users[newID] = {
@@ -210,42 +204,42 @@ app.post("/register", (req, res) => {
   }
 });
 
-// GET to 404 page
+// GET to 404 page.
 app.get("/404", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = { user: users[user_id] };
-  res.render("urls_404", templateVars)
+  res.render("urls_404", templateVars);
 });
 
-// POST to redirect from 404 page back to /register
+// POST to redirect from 404 page back to /register.
 app.post("/404", (req, res) => {
-  res.redirect("/register")
+  res.redirect("/register");
 });
 
-// GET to 403 page for incorrect email
+// GET to 403 page for incorrect email.
 app.get("/403-email", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = { user: users[user_id] };
-  res.render("urls_403-email", templateVars)
+  res.render("urls_403-email", templateVars);
 });
 
-// POST to redirect from 403 page back to /login
+// POST to redirect from 403 page back to /login.
 app.post("/403-email", (req, res) => {
-  res.redirect("/login")
+  res.redirect("/login");
 });
 
-// GET to 403 page for incorrect password
+// GET to 403 page for incorrect password.
 app.get("/403-password", (req, res) => {
   const templateVars = { users: users, user_id: req.session.user_id };
-  res.render("urls_403-Password", templateVars)
+  res.render("urls_403-Password", templateVars);
 });
 
-// POST to redirect from 403 page back to /login
+// POST to redirect from 403 page back to /login.
 app.post("/403-password", (req, res) => {
-  res.redirect("/login")
+  res.redirect("/login");
 });
 
-// App is LISTENING on port 8080
+// App is LISTENING on port 8080.
 app.listen(PORT, () => {
   console.log(`This app is listening on port ${PORT}`);
 });
